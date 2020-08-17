2bc0a6d20e96637392725491aa8b0769d364e130
tal007 <mrliu819@foxmail.com>
2020-08-17 14:13:57 +0800

    DEL：删除无用的

--- src/components/FooterConsumer.jsx
+++ src/components/FooterConsumer.jsx
@@ -1,22 +1,16 @@
-import { useEffect } from 'react';
 import ThemeContext from './ThemeContext';
 
-const FooterConsumer = () => {
-  useEffect(() => {
-    console.log('footer child');
-  });
-  return (
-    <ThemeContext.Consumer>
-      {(value) => (
-        <div>
-          <h2>
-            Context Consumer
-            {value}
-          </h2>
-        </div>
-      )}
-    </ThemeContext.Consumer>
-  );
-};
+const FooterConsumer = () => (
+  <ThemeContext.Consumer>
+    {(value) => (
+      <div>
+        <h2>
+          Context Consumer
+          {value}
+        </h2>
+      </div>
+    )}
+  </ThemeContext.Consumer>
+);
 
 export default FooterConsumer;
--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,10 +1,7 @@
-import Header from '@comp/Header';
-
 class Layout extends React.Component {
   render() {
     return (
       <section className="layout" style={{ margin: 20 }}>
-        <Header />
         {this.props.children}
       </section>
     );
